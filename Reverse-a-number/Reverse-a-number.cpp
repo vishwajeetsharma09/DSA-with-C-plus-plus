673a1416667909a4bba590b737653246006e96ac
@vishwajeetsharma09 <2000031822@kluniversity.in>
2024-02-20 02:14:36 +0530

    updated

--- Reverse-a-number/Reverse-a-number.cpp
+++ Reverse-a-number/Reverse-a-number.cpp
@@ -1,16 +1,32 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int reverse = 0;
+//     while (n)
+//     {
+//         int digit = n % 10;
+//         reverse = reverse * 10 + digit;
+//         n = n / 10;
+//     }
+//     cout<<reverse;
+//     return 0;
+// }
 #include <iostream>
 using namespace std;
 int main()
 {
-    int n;
-    cin >> n;
+    int n = 12345;
+    // cin >> n;
     int reverse = 0;
+    // for(int i=0 ;i<=n ;++i)
     while (n)
     {
         int digit = n % 10;
         reverse = reverse * 10 + digit;
         n = n / 10;
     }
-    cout<<reverse;
-    return 0;
+    cout << reverse;
 }
